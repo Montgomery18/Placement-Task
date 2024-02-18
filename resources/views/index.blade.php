8ea84aa27178c0616199977d0f91528239496708
Montgomery18 <c2026842@hallam.shu.ac.uk>
2024-02-18 16:33:27 +0000

    header html and css started

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,15 @@
     <title>Elanco Animal Tracker</title>
 
     <link href="{{ asset('css/desktop.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet" >
     <link media="only screen and (min-width:1027px)" href="" rel="stylesheet">
 </head>
 <body>
     <header>
+        <div class="titleHead">
+            <img src="{{ asset('images/Elanco_Logo.png') }}" alt="Elanco Logo">
+            <h1>Activity Tracker</h1>
+        </div>
         <nav>
             <ul>
                 <li><a href="/Login">Login</a></li>
