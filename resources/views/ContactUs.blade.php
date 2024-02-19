eb78ce5c548ee2bd2e7601fa1e0470cff51abaa6
Jamesyb03 <113983687+Jamesyb03@users.noreply.github.com>
2024-02-19 09:08:57 +0000

    small change

--- resources/views/ContactUs.blade.php
+++ resources/views/ContactUs.blade.php
@@ -5,18 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elanco Animal Tracker</title>
+
+    <link href="{{ asset('css/desktop.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet" >
+    <link media="only screen and (min-width:1027px)" href="" rel="stylesheet">
 
 </head>
 <body>
-    <header>
-        <nav>
-
-        </nav>
+     <header>
+        <div id="LeftHead">
+            <img src="{{ asset('images/Elanco_Logo.png') }}" alt="Elanco Logo">
+            <p><a href="/Login">Login</a></p>
+        </div>
+        <div id="RightHead">
+            <h1>Activity Tracker</h1>
+            <nav>
+                <ul>
+                    <li><a href="/Register">Register</a></li>
+                    <li><a href="/ResetPassRequest">Reset Password Request</a></li>                        
+                    <li><a href="/Profile">Profile</a></li>
+                    <li><a href="/Trends">Trends</a></li>
+                    <li><a href="/Admin">Admin</a></li>
+                    <li><a href="/ContactUs">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 
     <main>
         <section>
-           bvdhjbhjbhbhz
+           
         </section>
         <section>
 
@@ -24,9 +45,9 @@
     </main>
 
     
-     <footer  class="border-top footer">
+     <footer  class="footer border-top">
             <div class="container">
-                Footer
+                &copy;
             </div>
     </footer>
     
