2d1d1b3f6b84b6af91e52bebeccd51b74d54a02c
mrahman13 <116874917+mrahman13@users.noreply.github.com>
2024-02-20 08:39:38 +0000

    Adding admin links to admin page

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -1,23 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="{{ asset('css/Admin.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css/Header.css') }}" rel="stylesheet" >
+
+    <title>Admin</title>
 </head>
 <body>
     <header>
-        <nav>
-
-        </nav>
+    <div id="LeftHead">
+            <img src="{{ asset('images/Elanco_Logo.png') }}" alt="Elanco Logo">
+            <p><a href="/Login">Login</a></p>
+        </div>
+        <div id="RightHead">
+            <h1>Activity Tracker</h1>
+            <nav>
+                <ul>
+                    <li><a href="/Register">Register</a></li>
+                    <li><a href="/ResetPassRequest">Reset Password Request</a></li>                        
+                    <li><a href="/Profile">Profile</a></li>
+                    <li><a href="/Trends">Trends</a></li>
+                    <li><a href="/Admin">Admin</a></li>
+                    <li><a href="/ContactUs">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
-
     <main>
         <section>
-
-        </section>
-        <section>
-
+        <ul id="admin_list">
+                <li><a href="/AdminPassReset">Password Reset Management</a></li>
+                <li><a href="/AdminDeleteUser">Manage Users</a></li>
+            </ul>
         </section>
     </main>
 
