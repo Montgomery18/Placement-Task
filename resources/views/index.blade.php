5ae85601bed3f23318da0256a167f9c4532a07b9
Jamesyb03 <113983687+Jamesyb03@users.noreply.github.com>
2024-02-21 16:00:55 +0000

    updated css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Elanco Animal Tracker</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/desktop.css') }}" rel="stylesheet" >
     <link href="{{ asset('css/header.css') }}" rel="stylesheet" >
     <link media="only screen and (min-width:1027px)" href="" rel="stylesheet">
@@ -26,13 +26,16 @@
                   On this website you will be able to view data in graphical form as well as manipulate that data to your needs
                   This data will then provide trends seen in the data and show this either on the graph or in text form.
                   <div>
-                    To get started either login to your account or register for one with the nav bar above.
+                    To get started either login to your account or register for one  with the button below.
                   </div>
                 </div>
             </div>
         </section>
-        <section>
-
+        <section class="Register_button">
+            <form action="/Register" method="get">
+               <input class="btn form_button" type="submit" value="Register">
+            </form>
+         
         </section>
     </main>
 
