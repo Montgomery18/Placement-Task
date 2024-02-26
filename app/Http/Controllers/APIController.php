7716b5737a2d0490840fe96b51e50d5fd8864774
Montgomery18 <c2026842@hallam.shu.ac.uk>
2024-02-26 04:17:19 +0000

    data display completed, trends started

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -17,8 +17,41 @@ class APIController extends Controller
     }
 
     public function RetrieveData(Request $request){
-       $response = Http::get('https://engineering-task.elancoapps.com/api/applications/' . $request->ApplicationSelect);
-       $data = json_decode($response);
-       return view('data_page', ["appData" => $data, "appName" => $request->ApplicationSelect]);
+        if (!session()->has("AppData") || $request->newData == "true"){
+            session()->forget("AppData");
+            session()->forget("AppDataDisplay");
+            session()->forget("appName");
+            $response = Http::get('https://engineering-task.elancoapps.com/api/applications/' . $request->ApplicationSelect);
+            $data = json_decode($response);
+            session(["AppData" => $data, "AppDataDisplay" => 0, "appName" => $request->ApplicationSelect]);
+            return view('data_page'); // ["appData" => $data, "appName" => $request->ApplicationSelect]);
+        }
+        else if (isset($request->Nextwhere)){
+            $dataNumber = session()->get("AppDataDisplay");
+            if ($request->Nextwhere == "forward"){
+                if ($dataNumber + $request->NextFive <= count(session()->get("AppData"))){
+                    session()->forget("AppDataDisplay");
+                    session(["AppDataDisplay" => ($dataNumber + $request->NextFive)]);
+                    return view('data_page');
+                }
+                else{
+                    return view('data_page');
+                }
+            }
+            else if ($request->Nextwhere == "backward"){
+                if ($dataNumber - $request->NextFive >= 0){
+                    session()->forget("AppDataDisplay");
+                    session(["AppDataDisplay" => ($dataNumber - $request->NextFive)]);
+                    return view('data_page');
+                }
+                else{
+                    return view('data_page');
+                }
+            }
+
+        }
+        else{
+            return view('data_page');
+        }
     }
 }
--- resources/views/data_page.blade.php
+++ resources/views/data_page.blade.php
@@ -13,67 +13,101 @@
         </header>
         <main>
             <section>
+                <h2>View Data</h2>
                 <form id="Application" action="{{ route('selectData') }}" method="post">
                     @csrf
+                    <input style="display:none;" type="text" id="newData" name="newData" value="true">
                     <label>Select Application</label>
                     <select id="ApplicationSelect" name="ApplicationSelect" form="Application">
-                    @if (session()->has("ApplicationNames"))
-                        @if (isset($appName))
-                                <option value="{{ $appName }}">{{ $appName }}</option>
-                        @endif
-                        @foreach(session()->get("ApplicationNames") as $data)
-                            @if (isset($appName)){
-                                @if ($data != $appName)
-                                    <option value="{{ $data }}">{{ $data }}</option>
-                                @endif
+                    <?php
+                        if (session()->has("ApplicationNames")){
+                            if (session()->has("appName")){
+                                echo "<option value='" . session()->get("appName") . "'>" . session()->get("appName") . "</option>";
                             }
-                            @else
-                                <option value="{{ $data }}">{{ $data }}</option>
-                            @endif
-                        @endforeach
-                    @endif
+                            foreach(session()->get("ApplicationNames") as $data){
+                                if (session()->has("appName")){
+                                    if ($data != session()->get("appName")){
+                                        echo "<option value='" . $data . "'>" . $data . "</option>";
+                                    }
+                                }
+                                else{
+                                    echo "<option value='" .  $data . "'>" . $data . "</option>";
+                                }
+                            }
+                        }
+                    ?>
                     </select>
                     <input type="submit" value="Submit">
                 </form>
-                    <table>
-                        <tr>
-                            @if (isset($appData))
-                                <th>Application - {{ $appName }}</th>
-                            @else
-                                <th>Application</th>
-                            @endif
-                        </tr>
-                        @if (isset($appData))
-                            <tr>
-                                <th>Resources - {{ $appData[4]->ServiceName }}</th>
-                            </tr>
-                        @endif
-                        <tr>
-                            <th>Date</th>
-                            <th>Consumed Quantity</th>
-                            <th>Cost</th>
-                            @if (isset($appData))
-                                @foreach($appData as $displayData)
-                                    <tr>
-                                        <td>{{ $displayData->Date}}</td>
-                                        <td>{{ $displayData->ConsumedQuantity }}</td>
-                                        <td>{{ $displayData->Cost }}</td>
-                                    </tr>
-                                @endforeach
-                            @else
-                                <tr>
-                                    <td>p</td>
-                                    <td>p</td>
-                                    <td>p</td>
-                                </tr>
-                            @endif
-                    </table>
+                <?php
+                    if (session()->has("AppData")){
+                        $AppData = session()->get("AppData");
+                        echo "<h3>Application -" . session()->get("appName") . "</h3>";
+                        echo "<h3>Resources -" . $AppData[4]->ServiceName . "</h3>";
+                    }
+                    else{
+                        echo "<h3>Application - Waiting for input</h3>";
+                        echo "<h3>Resources - Waiting for input</h3>";
+                    }
+                ?>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Date</th>
+                        <th>Consumed Quantity</th>
+                        <th>Cost</th>
+                    </tr>
+                    <?php
+                        if (isset($AppData)){
+                            $countLimit = session()->get("AppDataDisplay") + 6;
+                            for ($i = session()->get("AppDataDisplay"); $i < $countLimit; $i++){
+                                if (isset($AppData[$i])){
+                                    echo "<tr>";
+                                    echo "<td>" . $i . "</td>";
+                                    echo "<td>" . $AppData[$i]->Date . "</td>";
+                                    echo "<td>" . $AppData[$i]->ConsumedQuantity . "</td>";
+                                    echo "<td>" . $AppData[$i]->Cost . "</td>";
+                                    echo "</tr>";
+                                }
+                            }
+                        }
+                        else{
+                            echo "<tr>";
+                            echo "<td>Awaiting User Choice</td>";
+                            echo "<td>Awaiting User Choice</td>";
+                            echo "<td>Awaiting User Choice</td>";
+                            echo "<td>Awaiting User Choice</td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </table>
+                <form class="BottomForm" action="{{ route('selectData') }}" method="post">
+                    @csrf
+                    <div>
+                        <label for="forward">Forward</label>
+                        <input type="radio" id="forward" name="Nextwhere" value="forward">
+                    </div>
+                    <div>
+                        <label for="backward">Backward</label>
+                        <input type="radio" id="backward" name="Nextwhere" value="backward">
+                    </div>
+                    <input type="number" min="1" max="5" id="NextFive" name="NextFive" value="1">
+                    <input type="submit" value="submit">
+                </form>
             </section>
             <section>
-                <p>placeholder</p>
-            </section>
-            <section>
-                <p>placeholder</p>
+                <h2>Data Tools</h2>
+                <form id="Tools" action="{{ route('selectData') }}" method="post">
+                    <label>Select Tool</label>
+                    <select id="ApplicationSelect" name="ApplicationSelect" form="Tools">
+                        <option value="HighestValue">Highest Cost</option>
+                        <option value="LowestValue">Lowest Cost</option>
+                        <option value="Average">Average Cost</option>
+                        <option value="Range">Range</option>
+
+                    </select>
+                    <input type="submit" value="submit">
+                </form>
             </section>
         </main>
 
